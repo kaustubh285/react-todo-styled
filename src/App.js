b1e58853c3adddab07904d1c796999df258cc19c
kaustubh <kaustubh285@gmail.com>
2020-07-24 12:11:27 +0530

    Added bootstrap and started using styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='form-check'>
+        <input type='checkbox' className='form-check-input' id='task1' />
+        <label className='form-check-label' htmlFor='task1'>
+          Task 1
+        </label>
+      </div>
+      <div className='form-check'>
+        <input type='checkbox' className='form-check-input' id='task2' />
+        <label className='form-check-label' htmlFor='task2'>
+          Task 2
+        </label>
+      </div>
+      <div className='form-check'>
+        <input type='checkbox' className='form-check-input' id='task3' />
+        <label className='form-check-label' htmlFor='task3'>
+          Task 3
+        </label>
+      </div>
     </div>
   );
 }
-
-export default App;
